e8a8553d6d1fdafb99a3349f01d07cd0a79b172b
easonhts <1500019760@qq.com>
2021-12-10 16:29:28 +0800

    feat: px to vw

---
+++ .postcssrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  plugins: {
+    'postcss-px-to-viewport': {
+      unitToConvert: 'px',
+      viewportWidth: 750,
+      unitPrecision: 6,
+      propList: ['*'],
+      viewportUnit: 'vw',
+      fontViewportUnit: 'vw',
+      selectorBlackList: [], // 转换黑名单，
+      minPixelValue: 1,
+      mediaQuery: true,
+      exclude: [/node_modules/],
+      landscape: false
+    }
+  }
+}
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,13 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+// const path = require('path')
+
+// const resolve = (dir) => path.join(__dirname, dir)
+
 module.exports = {
   devServer: {
     before: require('./mock/mock-server')
   }
+  // chainWebpack: (config) => {
+  //   config.resolve.alias.set('@', resolve('src'))
+  // }
 }
