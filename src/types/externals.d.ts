d261c64a2456bc13d467b02534794897d87549ee
easonhts <1500019760@qq.com>
2021-12-10 17:24:59 +0800

    feat: 初始化css

---
+++ src/types/externals.d.ts
@@ -0,0 +1,14 @@
+declare module '*.css'
+declare module '*.less'
+
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.gif';
+
+declare module '*.json'
+
+declare module '*.eot'
+declare module '*.svg'
+declare module '*.tff'
+declare module '*.woff'
